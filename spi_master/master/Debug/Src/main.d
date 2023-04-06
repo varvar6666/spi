4c758d74c6d6fa70dc2f9bee179d6e678c6762ba
Vlasov <vlasov.serge.m@gmail.com>
2023-04-06 17:08:48 +0300

    spi master and slave

---
+++ spi_master/master/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c \
+ ../../../libs/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ ../../../libs/CMSIS/Include/core_cm4.h \
+ ../../../libs/CMSIS/Include/cmsis_version.h \
+ ../../../libs/CMSIS/Include/cmsis_compiler.h \
+ ../../../libs/CMSIS/Include/cmsis_gcc.h \
+ ../../../libs/CMSIS/Include/mpu_armv7.h \
+ ../../../libs/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+../../../libs/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+../../../libs/CMSIS/Include/core_cm4.h:
+../../../libs/CMSIS/Include/cmsis_version.h:
+../../../libs/CMSIS/Include/cmsis_compiler.h:
+../../../libs/CMSIS/Include/cmsis_gcc.h:
+../../../libs/CMSIS/Include/mpu_armv7.h:
+../../../libs/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
